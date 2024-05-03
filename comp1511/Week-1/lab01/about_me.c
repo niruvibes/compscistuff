2a27226a5a853916497814051d915c6705a7690c
z5592479 <z5592479@vx08.orchestra.cse.unsw.EDU.AU>
2024-05-03 15:29:47 +1000

    finished week1 lab

---
+++ comp1511/Week-1/lab01/about_me.c
@@ -0,0 +1,20 @@
+// About Me
+//  about_me.c
+//
+// This program was written by Neil T (z5592479)
+// on 3/05/2024
+//
+// A program to tell us about yourself!
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("My favourite food is: Pizza\n");
+    printf("I'm doing COMP1511 because: i dont know\n");
+    printf("I hope to have achieved: learning to code :0 by the time I finish COMP1511.\n");
+    printf("Something I'm passionate about is: games\n");
+    printf("My question about COMP1511 that hasn't been answered is: why C\n");
+
+    return 0;
+}
